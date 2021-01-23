f3c572298f2016bd2a247e98b6543b2d7f7e7b95
MattPapesh <55628349+MattPapesh@users.noreply.github.com>
2021-01-22 22:02:42 -0600

    Changed string variables to char* variables

--- LCD_interface/LCD_interface.h
+++ LCD_interface/LCD_interface.h
@@ -10,12 +10,13 @@
     {
         bool mainFolder = false; 
 
-        String folderName, parentFolder;
+        char* folderName;
+        char* parentFolder;
         
-        String* listOfFoldersPTR = nullptr;
-        int* sizeOfListOfFoldersPTR = nullptr;
+        char** listOfFoldersPTR = nullptr;
+        short int* sizeOfListOfFoldersPTR = nullptr;
 
-        void createFolder(String tempListOfFolders[], int tempSizeOfListOfFolders);
+        void createFolder(char* tempListOfFolders[], short int tempSizeOfListOfFolders);
 
         ~folder();
     };
@@ -24,30 +25,34 @@
     {   
         private:// FOR LCD FUNCTIONALITY & MENU FUNCTIONALITY (allows menus to be navigated on the LCD with a dial encoder)
 
-        int row, prevRow;
+        short int row, prevRow;
         
         bool foldersBeforeFunctions = true; 
-        bool buttonPressed;
-        int const buttonDelayInMillis = 200; 
+        
+        short int const buttonDelayInMillis = 200; 
 
-        String arrow = "->", removeArrow = "  ";
-        String BACK = "< BACK >";
+        char* arrow = "->";
+        char* removeArrow = "  ";
 
         public: // PUBLIC VARIABLES FOR LCD SETUP AND MENU FUNCTIONALITY
 
-         String interfaceTitle = "**Main Menu**"; 
+         bool buttonPressed;
+
+         char* BACK = "< BACK >";
+
+         char* interfaceTitle = "**Main Menu**"; 
         
          int prevDialVal, dialVal;  
         
-         int LCD_resgisterSelectPin = 12, LCD_enablePin = 11, LCD_digitalPin0 = 5, LCD_digitalPin1 = 4, LCD_digitalPin2 = 8, LCD_digitalPin3 = 6,
+         int LCD_resgisterSelectPin = A5, LCD_enablePin = A4, LCD_digitalPin0 = A2, LCD_digitalPin1 = 7, LCD_digitalPin2 = 8, LCD_digitalPin3 = A3,
          LCD_displayColoumns = 16, LCD_displayRows = 2,
-         dial_SW_Pin = 2, dial_CLK_Pin = A0, dial_DT_Pin = A1, numberOfFolders;  
+         dial_SW_Pin = 2, dial_CLK_Pin = A0, dial_DT_Pin = A1, numberOfFolders;  //A0 then A1
 
-         String* listOfFoldersPTR; 
-         int* sizeOfListOfFoldersPTR = new int; // index size   
+         char** listOfFoldersPTR; 
+         short int* sizeOfListOfFoldersPTR = new short int; // index size   
          folder* currentFolderOpen; 
 
-        String currentFolderSelected;    
+        char* currentFolderSelected;    
              
         private: // PRIVATE METHODS AND OBJECTS FOR LCD & MENU FUNCTIONALITY 
 
@@ -57,9 +62,9 @@
         
         bool interfaceBlockade();
         
-        int printOptionHorizontalLocation(String tempFolder);
+        short int printOptionHorizontalLocation(char* tempFolder);
 
-        void printFolders(bool topRow, String listOfFolders[]);
+        void printFolders(bool topRow, char* listOfFolders[]);
 
         void printArrow();
 
@@ -81,13 +86,13 @@
 
         void updateFolders(folder* tempMainFolderPTR, folder* tempNewFolderPTR);
 
-        void currentFolder(String tempListOfFolders[], int tempSize, folder* tempCurrentFolderOpen);
+        void currentFolder(char* tempListOfFolders[], short int tempSize, folder* tempCurrentFolderOpen);
         
         public:// PUBLIC VARIABLES AND METHOD FOR ORGANIZING THE FOLDERS THAT MAKE UP MENUS
 
         folder** PTR_ofListOfFolderObjPTRs = nullptr;  // array of folder pointers (addresses to folders)
 
-        int* amountOfFolderObjPTRs = new int(1); 
+        short int* amountOfFolderObjPTRs = new short int(1); 
         
         void includeFolder(folder* tempParentFolderPTR, folder* tempChildFolderPTR, bool tempFoldersBeforeFunctions);
     };
